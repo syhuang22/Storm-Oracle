4d933a303d496e7e5d7499e839a3b8784ee13435
Shao-yu Huang <syhuang1201@gmail.com>
2024-06-02 20:28:01 +0000

    raw gsod to s3

--- scripts/download/gsod_s3.py
+++ scripts/download/gsod_s3.py
@@ -5,29 +5,6 @@ import boto3
 import logging
 from utils import s3_client, BUCKET_NAME, clear_log_file, clear_s3
 
-# # AWS configuration
-# ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
-# SECRET_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-# BUCKET_NAME = 'my-storm-oracle-data'
-
-# # Initialize S3 client
-# s3_client = boto3.client(
-#     's3',
-#     aws_access_key_id=ACCESS_KEY,
-#     aws_secret_access_key=SECRET_KEY
-# )
-
-# def clear_log_file(log_file):
-#     open(log_file, 'w').close()
-#     print("Log file cleared")
-
-# def clear_s3_gsod(bucket_name, s3_path_prefix):
-#     response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=s3_path_prefix)
-#     if 'Contents' in response:
-#         for obj in response['Contents']:
-#             s3_client.delete_object(Bucket=bucket_name, Key=obj['Key'])
-#         print("S3 gsod/ directory cleared")
-
 def download_gsod_data(year, bucket_name, s3_path_prefix):
     base_url = f'https://www.ncei.noaa.gov/data/global-summary-of-the-day/access/{year}/'
     
